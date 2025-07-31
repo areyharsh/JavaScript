ee71b82f133412a469f803bfdaa648086b6084c3
Harshvardhan Deshmukh <hsdeshmukh1405@gmail.com>
2025-07-31 02:35:27 +0000

    DataTypes

---
+++ basics_101/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 1254
+let accountEmail = "arey@gmail.com"
+var accountPassword = "heheh"
+accountCity = "Maharashtra"
+let accountState;
+// never use  var due to its block scope issues
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
