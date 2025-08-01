7fe281af7b64c2e6bc2473825828b75e8c4189ee
Harshvardhan Deshmukh <hsdeshmukh1405@gmail.com>
2025-08-01 07:52:50 +0000

    practised

--- basics_101/06_string.js
+++ basics_101/06_string.js
@@ -7,34 +7,34 @@ const gameName = new String ("blackDiamond")
 
 console.log(gameName[5]);
 
-// console.log(gameName.__proto__);
+console.log(gameName.__proto__);
 
-// console.log(gameName.length);
+console.log(gameName.length);
 
-// console.log(gameName.toUpperCase());
+console.log(gameName.toUpperCase());
 
-// console.log(gameName.charAt(6));
+console.log(gameName.charAt(6));
 
-// console.log(gameName.indexOf('c'));
+console.log(gameName.indexOf('c'));
 
-// console.log(gameName.charAt(6));
+console.log(gameName.charAt(6));
 
-// console.log(gameName.substring(6));
+console.log(gameName.substring(6));
 
-// console.log(gameName.charAt(6));
+console.log(gameName.charAt(6));
 
-// const newString = gameName.substring(0,6)
-// console.log(newString);
+const newString = gameName.substring(0,6)
+console.log(newString);
 
-// const anotherString = gameName.slice(-7,3)
-// console.log(anotherString);
+const anotherString = gameName.slice(-7,3)
+console.log(anotherString);
 
-// const newStringOne = "  harsh     "
-// console.log(newStringOne.trimEnd());
+const newStringOne = "  harsh     "
+console.log(newStringOne.trimEnd());
 
-// const url = "https://harshya.com/harsh%20Deshmukh" 
-// console.log(url.replace('%20','_'));
+const url = "https://harshya.com/harsh%20Deshmukh" 
+console.log(url.replace('%20','_'));
 
-// console.log(url.includes('harsh','_'));
+console.log(url.includes('harsh','_'));
 
 console.log(gameName.split('-'));
