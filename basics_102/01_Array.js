b6dac7dcca9a8714ad0e6f90265e9bd072097d5f
Harshvardhan Deshmukh <hsdeshmukh1405@gmail.com>
2025-08-10 01:52:19 +0000

    Array

---
+++ basics_102/01_Array.js
@@ -0,0 +1,39 @@
+// Array
+
+const myArray = [0,1,2,3,4]
+
+//array is an object in JS
+
+// const myArr2 = new Array(1,2,3,4)
+// console.log(myArray[1]);
+
+// Array methods
+
+// myArray.push(32)
+// myArray.push(21)
+// myArray.pop(32)
+//console.log(myArray);
+
+// myArray.unshift(2)
+// myArray.shift()
+// console.log(myArray);
+//unshift adds the given number to 
+// the first place of the array
+//shift undo the work of unshift
+
+// console.log(myArray.includes(8));
+// console.log(myArray);
+
+// const newArr = myArray.join()
+
+// console.log(myArray);
+// console.log(newArr);
+
+
+//slice,splice
+
+var user = ["harsh", "malak", "durga", "adya","akka"]
+console.log( user.slice(1,3));
+
+user.splice(1,3,"Missing ppl")
+console.log(user);
